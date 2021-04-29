9cd3d148860230160022d19f930cc88b79ec4035
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-29 18:06:38 -0400

    adds li element

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
 function addEventListener() {
   let form = document.getElementById('create-task-form')
   form.addEventListener('submit', (event) => {
+    console.log(event)
     event.preventDefault()
+    addPTag(event.target['new-task-description'].value)
 
 })}
 
 
+function addPTag (content) {
+  let li = document.createElement('li')
+  let tasks = document.getElementById('tasks')
+  //let button = document.createElement('button')
+  li.textContent = content
+  tasks.appendChild(li)
+  //li.appendChild(button)
+}
+addEventListener()
 
